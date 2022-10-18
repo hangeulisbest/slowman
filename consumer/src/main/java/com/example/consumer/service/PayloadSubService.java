38e82e0a68004f48797f27fa0eb8ee63246f067e
bern.cty <bern.cty@kakaobank.com>
2022-10-18 18:36:14 +0900

    [FEAT] Flux SSE 테스트3

--- consumer/src/main/java/com/example/consumer/service/PayloadSubService.java
+++ consumer/src/main/java/com/example/consumer/service/PayloadSubService.java
@@ -1,5 +1,8 @@
 package com.example.consumer.service;
 
+import com.example.core.model.Payload;
+import com.example.core.util.ObjectMapperUtils;
+import java.util.Map.Entry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
@@ -12,13 +15,31 @@ import org.springframework.stereotype.Service;
 public class PayloadSubService {
 
     private final ReactiveRedisOperations<String,Object> redisOperations;
-    private static final int TPS = 10;
+    private static final int TPS = 10000;
+
+//    @Scheduled(fixedDelay = 1000)
+//    public void subscribe() throws InterruptedException{
+//        redisOperations.opsForHash().size("payload")
+//                        .subscribe(o->{
+//                            log.info("PAYLOAD SIZE = {}",o);
+//                        });
+//    }
 
     @Scheduled(fixedDelay = 1000)
-    public void subscribe() throws InterruptedException{
-        redisOperations.opsForHash().size("payload")
-                        .subscribe(o->{
-                            log.info("PAYLOAD SIZE = {}",o);
-                        });
+    public void consumeTest() {
+        redisOperations.opsForHash().randomEntries("payload",TPS)
+            .map(o->ObjectMapperUtils.objectMapper(o.getValue(), Payload.class)
+            )
+            .flatMap(o->{
+                return redisOperations.opsForHash().remove("payload",o.getId()).map(t->o);
+            }).subscribe(
+                o->{
+                    // webClient를 통해 다른곳으로 보낼수 있지 않을까????
+                    //log.info("PAYLOAD {} IS DELETED ",o);
+                }
+            );
+
+
+
     }
 }
--- publisher/src/test/java/com/example/publisher/PublisherApplicationTests.java
+++ publisher/src/test/java/com/example/publisher/PublisherApplicationTests.java
@@ -1,9 +1,15 @@
 package com.example.publisher;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 @SpringBootTest
+@EntityScan(basePackages = {"com.example.core","com.example.publisher"})
 class PublisherApplicationTests {
 
 	@Test
